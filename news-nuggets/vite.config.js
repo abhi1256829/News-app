3fcbc1e64f5790aa54fb345c764a128e6af4554e
abhi1256829 <ag1256829@gmail.com>
2025-08-22 03:14:19 +0530

    Update vite.config.js

--- news-nuggets/vite.config.js
+++ news-nuggets/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 5173 // Change to any port you prefer (e.g., 3000)
